222ec88df2999c20b6f27e73de9fb3a94c872ff6
Atimq <159470505+Atimq@users.noreply.github.com>
2024-02-09 07:59:41 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function gloria(email,password){
+const validEmail = "atimgloria223@gmail.com"
+const validPassword ="2200721522@2024"
+
+if(email =validEmail , password = validPassword){
+    console.log('user email is{email},login successfully%');
+} else 
+{
+    console.log ('incorrect user credentials');
+
+}
+
+}
+gloria('atimgloria@gmail.com','2200721522@2024')
